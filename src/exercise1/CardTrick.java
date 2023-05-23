4003050d00785c719016af979618fe17cfecc873
Rathnayake Rathnayake <rathnayar@sheridancollege.ca>
2023-05-23 16:11:42 -0400

    Im done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -57,6 +57,8 @@ public class CardTrick {
      * author Rathnayake Rathnayake May 23 2023
      */
     private static void printInfo() {
+        
+        //printinfo
 
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
